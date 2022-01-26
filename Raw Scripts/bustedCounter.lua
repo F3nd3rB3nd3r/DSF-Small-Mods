99687eab00f24318f2b75ca1821afa70390cc8ac
Bence Gál <88badassshark88@gmail.com>
2022-01-26 02:02:09 +0100

    Busted! Mod upload
    Remove of comments

--- Raw Scripts/bustedCounter.lua
+++ Raw Scripts/bustedCounter.lua
@@ -14,7 +14,6 @@ local maximumBustedSpeed = 5
 local promptActive = false
 local oneSecRate = 100 / bustedTime
 function getawayBustedCounter()
-    print("getawayBustedCounter")
     local getawayGameVehicle = localPlayer.primaryFelony.getawayGameVehicle
     if copCars[localPlayer.currentVehicle.gameVehicle.model_id] and
     getawayGameVehicle.speed < maximumBustedSpeed and
@@ -24,12 +23,11 @@ function getawayBustedCounter()
     end
     if effectActive and startTime then
         if not promptActive then
-             --feedbackSystem.menusMaster.masterSetVariable("iPrompt_M_Secondary_Display", 1)
 			 feedbackSystem.menusMaster.masterSetVariable("iScore_Feedback_Gauge_State", 1)
+			feedbackSystem.menusMaster.masterSetTextVariable("score_feedback_gauge", "BUSTED")
              promptActive = true
         end
-        --feedbackSystem.menusMaster.masterSetTextVariable("prompt_secondary", string.format("BUSTED IN %.0d",startTime + bustedTime - g_NetworkTime + 1))
-		feedbackSystem.menusMaster.masterSetVariable("iScore_Feedback_Gauge", (startTime + bustedTime - startTime) * oneSecRate)
+		feedbackSystem.menusMaster.masterSetVariable("iScore_Feedback_Gauge", (startTime + bustedTime - g_NetworkTime) * oneSecRate)
     end
     if effectActive and startTime and g_NetworkTime > startTime + bustedTime then
         bustedCounterCleanUp()
@@ -37,7 +35,6 @@ function getawayBustedCounter()
     if not distanceCalculation(getawayGameVehicle,bustedRadius) or getawayGameVehicle.speed > maximumBustedSpeed or not copCars[localPlayer.currentVehicle.gameVehicle.model_id] then
         effectActive = false
         if promptActive then
-            --feedbackSystem.menusMaster.masterSetVariable("iPrompt_M_Secondary_Display", 0)
 			feedbackSystem.menusMaster.masterSetVariable("iScore_Feedback_Gauge_State", 2)
             promptActive = false
         end
@@ -45,10 +42,6 @@ function getawayBustedCounter()
     if getawayGameVehicle.damage > 0.995 or not Chase.IsAChaseActive() then
         bustedCounterCleanUp()
     end
-end
-
-function bustedBar(show)
-
 end
 
 function distanceCalculation(getawayGameVehicle,bustedRadius)
@@ -61,7 +54,6 @@ end
 
 function bustedCounterCleanUp()
     local getawayGameVehicle = localPlayer.primaryFelony.getawayGameVehicle
-    --feedbackSystem.menusMaster.masterSetVariable("iPrompt_M_Secondary_Display", 0)
     feedbackSystem.menusMaster.masterSetVariable("iScore_Feedback_Gauge_State", 0)
     felony_chase.wrecked(getawayGameVehicle)
     getawayGameVehicle.damage = 1
